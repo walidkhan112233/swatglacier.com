25561d87fd71949e6cfbcf1478e8dbc87ee1657b
Walid khan <walidkhan112233@users.noreply.github.com>
2026-02-07 19:47:38 +0500

    7feb

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Routes,Route, useLocation } from 'react-router-dom';
 
 const App = () => {
 
@@ -12,6 +12,12 @@ const App = () => {
 
       { !isOwnerPath && <Navbar setShowLogin ={setShowLogin} /> }
       
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cooler-details/:id" element={<coolerDetails />} />
+      <Route path="/my-bookings" element={<myBookings />} />
+      <Route path="/coolers" element={<Coolers />} />
+    </Routes>
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center ' >
+
+
+      <h1 className='text-4xl md:text-5xl font-semibold' >Stay Cool. Stay Refreshed.</h1>
+      <br />
+      <h3 className='text-4xl md:text-5xl font-semibold' >Complete Renting & Sales Solutions by Swat Glacier</h3>
+ 
+    </div>
+  )
+}
+
+export default Hero
